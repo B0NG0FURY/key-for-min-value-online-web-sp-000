96a3d759a94f09259886eba8c8e31b608f103656
John Troutman <jtroutmanman@gmail.com>
2020-09-21 22:53:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,14 +7,16 @@ def key_for_min_value(name_hash)
   else
    name_array = name_hash.collect {|name, num| name}
    num_array = name_hash.collect {|name, num| num}
-   lowest = num_array[0]
    low_name = name_array[0]
-   num_array.each do |num|
-     if num < lowest
-       lowest = num
-       low_name = name_array[num]
+   low_num = num_array[0]
+   i = 1
+   while i < num_array.length
+     if num_array[i] < low_num
+       low_num = num_array[i]
+       low_name = name_array[i]
+       i += 1
      end
    end
-  end
-  low_name
+   end
+   low_name
 end
